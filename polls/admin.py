09d4ebe9fe38891b5beb2081280ecb2928600da3
hardvic <hardvic@outlook.com>
2016-08-25 11:39:35 +0800

    学习 part7 部分

--- polls/admin.py
+++ polls/admin.py
@@ -3,10 +3,29 @@
 
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
